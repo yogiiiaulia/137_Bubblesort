1a4c8cd0f7e1aec0f4fdec5aedab9c2a9adbb6e5
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-03-13 17:34:34 +0700

    add procedur input procedur bubblesort

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,64 +1,73 @@
+
 #include <iostream>
 using namespace std;
 
-int arr [15];
+int arr[15];
 int n;
 
 void inputData(){
+
     while (true)
     {
-        cout << "Masukan jumlah data : ";
+        cout << "Masukan Jumlah data: ";
         cin >> n;
 
-        if (n < 15)
-        {
+        if (n < 15){
             break;
         }
-        else
-        {
-            cout << "Jumlah data maksimal 15" << endl;        }
+        else{
+            cout << "Jumlah data maksimal 15";
+        }
     }
 
-    cout << "===========" << endl;
-    cout << "Masukan data" << endl;
-    cout << "===========" << endl;
+    cout << "=============" << endl;
+    cout << "Masukan Data" << endl;
+    cout << "=============" << endl;
     cout << endl;
 
-    for (int i =0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout << "Masukan data ke- " << i + 1 << " = ";
-        cin >> arr[i];
+        cout << "Masukan data ke-" << i + 1 << " = ";
+        cin >> arr [i];
     }
 }
 
-void Bubblesort() 
-{
-    int pass = 1;
-
-    do 
+void bubbleSort() {
+    int pass = 1; //step 1
+    do {
+        for (int j = 0; j <= n - 1 - pass; j++) //step 2   
     {
-    for (int j = 0; j <= n - 1 - pass; j++ )
-     {
-        if (arr[j] > arr [j+1]) 
+        if(arr[j] > arr [j+1]) //step 3
         {
             int temp;
-            temp = arr [j+1];
-            arr[j + 1] = temp;
-        
-        };
-       
-     };
+            temp = arr [j];
+            arr [j] = arr [j + 1];
+            arr [j + 1] = temp;
+        }
+    } 
 
-    pass = pass + 1;
-    while (pass <= n - 1);
-    }
- 
+    pass = pass + 1; //step 4
+    } while (pass <= n - 1); //step 5
+    
 }
 
+void displaysData()
+{
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Data setelah diurutkan" << endl;
+    cout << "==========================" << endl;
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << i + 1 << " = " << arr[i] << endl;
+    }
+}
 int main()
 {
-    inputData(); 
+    inputData();
+    bubbleSort();
+    displaysData();
+    return 0;
 }
-
-
-
